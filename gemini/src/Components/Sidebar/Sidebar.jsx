3272a891a55ea14b4197b62004de6ec5eedb9c45
jothi <jothilakshmi2288@gmail.com>
2024-05-29 05:23:45 -1200

    added new feature

--- gemini/src/Components/Sidebar/Sidebar.jsx
+++ gemini/src/Components/Sidebar/Sidebar.jsx
@@ -47,20 +47,22 @@ function Sidebar() {
           </div>
         )}
       </div>
-      <div className="bottom">
-        <div className="bottom-item recent-entry">
-          <img src={assets.question_icon} alt="" />
-          {extended && <p>Help</p>}
+      {previousPrompt.length <= 7 && (
+        <div className="bottom">
+          <div className="bottom-item recent-entry">
+            <img src={assets.question_icon} alt="" />
+            {extended && <p>Help</p>}
+          </div>
+          <div className="bottom-item recent-entry">
+            <img src={assets.history_icon} alt="" />
+            {extended && <p>Activity</p>}
+          </div>
+          <div className="bottom-item recent-entry">
+            <img src={assets.setting_icon} alt="" />
+            {extended && <p>Settings</p>}
+          </div>
         </div>
-        <div className="bottom-item recent-entry">
-          <img src={assets.history_icon} alt="" />
-          {extended && <p>Activity</p>}
-        </div>
-        <div className="bottom-item recent-entry">
-          <img src={assets.setting_icon} alt="" />
-          {extended && <p>Settings</p>}
-        </div>
-      </div>
+      )}
     </div>
   );
 }
